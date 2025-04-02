112f005427920a0d06cb2055bf5cf31329f0a706
ChartFly <f.moellers@comcast.net>
2025-04-02 11:23:58 -0400

    General update and bug fix

--- static/admin/market-holidays/MarketHolidays.js
+++ static/admin/market-holidays/MarketHolidays.js
@@ -81,8 +81,8 @@
               cloned.classList.add("editing");
 
               const cells = cloned.querySelectorAll("td:not(.col-select)");
-              cells.forEach((cell, index) => {
-                cell.innerText = index === 0 ? "New line, edit & save" : "";
+              cells.forEach(cell => {
+                cell.innerText = "";
                 cell.setAttribute("contenteditable", "true");
                 cell.classList.add("editable");
               });
@@ -103,7 +103,7 @@
               newRow.classList.add("editing");
               newRow.innerHTML = `
                 <td class="col-select"><input type="checkbox" class="holiday-select-checkbox" data-id="${newId}"></td>
-                <td contenteditable="true" class="editable">New line, edit & save</td>
+                <td contenteditable="true" class="editable"></td>
                 <td contenteditable="true" class="editable">YYYY-MM-DD</td>
                 <td contenteditable="true" class="editable">Upcoming</td>
                 <td contenteditable="true" class="editable">13:00</td>
