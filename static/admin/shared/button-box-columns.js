1d738ac02dced84bc467abdf9f0097ff8d63a43c
ChartFly <f.moellers@comcast.net>
2025-04-16 20:06:27 -0400

    Stop header from turning orange

--- static/admin/shared/button-box-columns.js
+++ static/admin/shared/button-box-columns.js
@@ -84,7 +84,6 @@ window.ButtonBoxColumns = (() => {
 
         clearActiveColumn(table);
         state.activeEditableColumnIndex = index;
-        header.classList.add("editable-col");
 
         const rows = table.querySelectorAll("tbody tr");
         rows.forEach((row) => {
